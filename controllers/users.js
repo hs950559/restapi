efa1f6b6ccc08ae0d1cb444f02f7fa53a90430f5
Hemant Kumar Singh <hemantkumarsingh@Hemants-MacBook-Air.local>
2018-02-27 15:20:01 +0530

    Api starter with get/post (promise based)

---
+++ controllers/users.js
@@ -0,0 +1,25 @@
+const User = require('../models/user');
+
+module.exports = {
+    index: (req, res, next) => {
+        User.find({})
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch((err) => {
+            next(err);
+        });
+    },
+
+    create: (req, res, next) => {
+    	const newUser = new User(req.body);
+
+    	newUser.save()
+		.then(user => {
+			res.status(201).json(user);
+		})
+		.then(err => {
+			next(err);
+		});
+    }
+};
---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const logger = require('morgan');
+const port = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/mydb');
+
+const app = express();
+
+// Routes
+const users = require('./routes/users');
+
+// Middlewares
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Routes
+app.use('/api/users', users);
+
+// Catch 404 errors and forword them to error handler
+app.use((req, res, next) => {
+	const err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
+
+// Error handler
+app.use((err, req, res, next) => {
+	const error = app.get('env') === 'development' ? err : {};
+	const status = error.status || 500;
+
+	res.status(status).json({
+		error: {
+			message: error.message
+		}
+	});
+});
+
+app.get('/', (req, res) => {
+	res.json({name: "koool"});
+});
+
+app.listen(port, ()=>{
+	console.log("Server Running http://localhost:"+port );
+});
+
